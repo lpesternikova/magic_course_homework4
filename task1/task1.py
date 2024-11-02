f1d5b37f960e1acf44db4429b1649f0d2f170d91
lpesternikova <lpesternikova@gmail.com>
2024-11-02 22:09:23 +0500

    task1

--- task1/task1.py
+++ task1/task1.py
@@ -4,6 +4,14 @@
 # а внутри — папки для жанров: fiction, non-fiction,
 # history, fantasy. Это будет ваш цифровой архив книг.
 
+import os
 
 def create_library():
+    os.mkdir("library")
+    os.makedirs("library/fiction")
+    os.makedirs("library/non-fiction")
+    os.makedirs("library/history")
+    os.makedirs("library/fantasy")
     pass
+
+create_library()
